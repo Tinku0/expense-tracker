6ba9b2a7e6bcc07da2ab872aef37071918dd913b
varun <varunchowdary23@gmail.com>
2025-01-07 10:22:12 +0530

    default categories loaded routes added

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const { connectToDB } = require('./config/connectToDatabase');
 const userRoutes = require('./routes/user');
 const expenseRoutes = require('./routes/expense');
 const profileRoutes = require('./routes/userprofile');
+const categoryRoutes = require('./routes/category');
 const verifyToken = require('./middleware/verifyToken');
+const seedDefaultCategories = require('./utils/setUpCategory');
 require('dotenv').config();
 
 const app = express();
@@ -16,8 +18,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("server is running on - ", PORT);
     connectToDB();
+    seedDefaultCategories();
 });
 app.use(express.json())
 app.use('/auth', userRoutes)
 app.use('/expense', verifyToken, expenseRoutes)
 app.use('/profile', verifyToken, profileRoutes)
+app.use('/category', verifyToken, categoryRoutes)
